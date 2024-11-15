0834ec3611ae181b7aa17fb7aab2ceb6f33cb46f
tahmid-kazi <101431441+tahmid-kazi@users.noreply.github.com>
2024-11-14 21:31:53 -0500

    leetcode 23 - merge k sorted lists done, beats 93%

---
+++ temp/l23-p1.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        # 11/14/24) Thurs) 915 to 929pm) tk) (still in makerlab!)
+        min_heap = []
+        for l in lists:
+            if l: #fill the minheap
+                heapq.heappush(min_heap, (l.val, id(l), l)) # revisit
+        dummy = ListNode()
+        current = dummy
+        while min_heap:
+            val, _, node = heapq.heappop(min_heap) #pop from the heap
+            current.next = node #and add it to the linkedlist
+            current = current.next
+            if node.next:
+                heapq.heappush(min_heap, (node.next.val, id(node.next), node.next))
+        return dummy.next
