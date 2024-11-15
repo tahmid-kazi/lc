03c7ce2d4b2c72d4f60acfdb28275b6826b6226f
tahmid-kazi <101431441+tahmid-kazi@users.noreply.github.com>
2024-11-14 20:56:43 -0500

    leetcode 347 - top k frequent elements done, beats 100%

---
+++ temp/l347-p1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # 11/14/24) Thurs) 833-854pm) tk) (in g14 + maker lab!)
+        # if k=2, return the top 2 most frequent elements
+        frequency = Counter(nums) #O(n)
+        result = heapq.nlargest(k, frequency.keys(), key = frequency.get) #O(n log k)
+        return result
