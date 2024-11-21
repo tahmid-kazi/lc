45a7068cd8d3003f6838254de3c6a59385bb3e33
tahmid-kazi <101431441+tahmid-kazi@users.noreply.github.com>
2024-11-20 21:25:39 -0500

    leetcode 2667 - JavaScript done beats 65%, gpt

---
+++ l2667-p1.py
@@ -0,0 +1,14 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    // 11/20/24) 922-924pm) Wed) tk)
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
