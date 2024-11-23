1e590bab63044a464152d48eb662eda091ba7470
tahmid-kazi <101431441+tahmid-kazi@users.noreply.github.com>
2024-11-22 23:53:47 -0500

    leetcode 303 - range sum query immutable done, beats 20%, sols, need to optimize

---
+++ l303-p1.py
@@ -0,0 +1,12 @@
+class NumArray:
+    # 11/22/24) Fri) 1149-1150pm) 1st and only problem of the day done
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+
+    def sumRange(self, left: int, right: int) -> int:
+        return sum(self.nums[left:right+1])
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
